92a82d44ba9c08c83ed820deca5470e23baac5e6
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-01-21 18:02:21 -0300

    feat: create http server and method adapter

---
+++ src/infra/http/AdapterExpress.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+import HttpServer from "./HttpServer";
+
+export default class AdapterExpress implements HttpServer {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  app: any;
+  constructor() {
+    this.app = express();
+    this.app.use(express.json());
+  }
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  register(method: string, url: string, callback: Function): void {
+    return this.app[method](url, async function (req: Request, res: Response) {
+      try {
+        const { statusCode, body } = await callback(req.query);
+        res.status(statusCode).json(body);
+      } catch (error) {
+        if (error instanceof Error) res.status(400).json(error.message);
+        res.status(400).json("Unexpected error");
+      }
+    });
+  }
+  listen(port: number): void {
+    this.app.listen(port, () => {
+      console.log(`SERVER RUNNING IN PORT ${port}`);
+    });
+  }
+}
