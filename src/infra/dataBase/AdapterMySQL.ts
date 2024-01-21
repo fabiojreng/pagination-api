15677e06ecb3dfffd05ef06994ee3452aaf4c167
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-01-21 17:58:29 -0300

    feat: create methods that connect to the database

---
+++ src/infra/dataBase/AdapterMySQL.ts
@@ -0,0 +1,40 @@
+import IDatabaseConnection from "./Connection";
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
+
+export default class AdapterMySQL implements IDatabaseConnection {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  connection: any;
+  constructor() {}
+
+  async connect(): Promise<void> {
+    try {
+      this.connection = mysql.createPool({
+        //connectionLimit: 10,
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        port: Number(process.env.PORT),
+      });
+      console.log("Conectado MySQL");
+    } catch (error) {
+      if (error instanceof Error) throw new Error(error.message);
+      throw new Error("Não foi possível conectar");
+    }
+  }
+
+  async close(): Promise<void> {
+    await this.connection.end();
+    console.log("Fechando MySQL");
+  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async query(statement: string, params: any): Promise<any> {
+    try {
+      return await this.connection.query(statement, params);
+    } catch (error) {
+      if (error instanceof Error) throw new Error(error.message);
+      throw new Error("Erro ao executar a query");
+    }
+  }
+}
