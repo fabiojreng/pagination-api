3a7de5cb44ad8158947916df5ec74538fb97c617
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-01-22 10:04:10 -0300

    feat: update query in data base

--- src/infra/repository/PaginationDB.ts
+++ src/infra/repository/PaginationDB.ts
@@ -4,13 +4,17 @@ import IDatabaseConnection from "../dataBase/Connection";
 
 export default class PaginationDB implements PaginationRepository {
   constructor(private db: IDatabaseConnection) {}
-  async findByAuthors(author: string, page: number): Promise<any> {
+  async filterDocuments(
+    type: string,
+    value: string,
+    page: number
+  ): Promise<any> {
     try {
       await this.db.connect();
       const query =
-        "SELECT * FROM repository.docs WHERE author LIKE ? LIMIT 5 OFFSET ?";
+        "SELECT * FROM repository.docs WHERE ?? LIKE ? AND status = 'available' LIMIT 5 OFFSET ?";
       const offset = page * 5;
-      const output = await this.db.query(query, [`%${author}%`, offset]);
+      const output = await this.db.query(query, [type, `%${value}%`, offset]);
       return output[0];
     } catch (error) {
       if (error instanceof Error) throw new Error(error.message);
