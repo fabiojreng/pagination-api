57e05bce66772a2031b784e399dcc0b7e639477c
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-01-24 16:30:32 -0300

    feat: add the use case of document filter by ID

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { FilterDocumentsUseCase } from "./application/useCases/FilterDocumentsUseCase";
+import getDocumentByIdUseCase from "./application/useCases/GetDocumentByIdUseCase";
 import MainController from "./infra/controller/MainController";
 import AdapterMySQL from "./infra/dataBase/AdapterMySQL";
 import AdapterExpress from "./infra/http/AdapterExpress";
@@ -8,7 +9,8 @@ const server = new AdapterExpress();
 const connection = new AdapterMySQL();
 const mysql = new PaginationDB(connection);
 const filterDocuments = new FilterDocumentsUseCase(mysql);
+const getById = new getDocumentByIdUseCase(mysql);
 
-new MainController(filterDocuments, server);
+new MainController(filterDocuments, getById, server);
 
 server.listen(3000);
