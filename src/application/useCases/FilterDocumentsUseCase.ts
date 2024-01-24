bfe766283428199dc0ebb53aa54568fb2908af24
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-01-24 16:23:04 -0300

    Update FilterDocumentsUseCase.ts

--- src/application/useCases/FilterDocumentsUseCase.ts
+++ src/application/useCases/FilterDocumentsUseCase.ts
@@ -3,8 +3,16 @@ import PaginationRepository from "../repository/PaginationRepository";
 export class FilterDocumentsUseCase {
   constructor(private repository: PaginationRepository) {}
 
-  execute(type: string, value: string, page: number): Promise<void> {
-    const pag = Number(page);
-    return this.repository.filterDocuments(type, value, pag);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async execute(type: string, value: string, page: number): Promise<any> {
+    try {
+      // Permitir apenas types: author, date_publication, type_document, area_CNPQ
+
+      const pag = Number(page);
+      return await this.repository.filterDocuments(type, value, pag);
+    } catch (error) {
+      if (error instanceof Error) throw new Error(error.message);
+      throw new Error("");
+    }
   }
 }
