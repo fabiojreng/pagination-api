25706bb885052f0f1be6aa55b03911a2292dd2d7
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-01-24 16:25:35 -0300

    feat: add search route by id

--- src/infra/controller/MainController.ts
+++ src/infra/controller/MainController.ts
@@ -1,19 +1,30 @@
 import { FilterDocumentsUseCase } from "../../application/useCases/FilterDocumentsUseCase";
+import getDocumentByIdUseCase from "../../application/useCases/GetDocumentByIdUseCase";
 import HttpServer from "../http/HttpServer";
 
 export default class MainController {
   constructor(
     private filterDocuments: FilterDocumentsUseCase,
+    private getById: getDocumentByIdUseCase,
     private server: HttpServer
   ) {
     server.register(
       "get",
       "/search",
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      async function (query: any) {
-        const { type, value, page } = query;
+      async function (req: any) {
+        const { type, value, page } = req.query;
         const documents = await filterDocuments.execute(type, value, page);
         return { statusCode: 200, body: documents };
+      }
+    );
+    server.register(
+      "get",
+      "/getById/:id",
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      async function (req: any) {
+        const document = await getById.execute(req.params.id);
+        return { statusCode: 200, body: document };
       }
     );
   }
