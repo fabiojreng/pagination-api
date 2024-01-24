c4835b0faf9a5439adb2eeb9a68f4971c80ce1d7
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-01-24 16:24:00 -0300

    feat: add method of filter by id

--- src/application/repository/PaginationRepository.ts
+++ src/application/repository/PaginationRepository.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export default interface PaginationRepository {
   filterDocuments(type: string, value: string, page: number): Promise<any>;
+  filterDocumentById(id: string): Promise<any>;
 }
