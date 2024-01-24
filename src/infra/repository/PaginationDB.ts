3200b3b14542ec8c155c8fd464e7c6c2a43c1686
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-01-24 16:28:54 -0300

    feat: implements method filter document by id

--- src/infra/repository/PaginationDB.ts
+++ src/infra/repository/PaginationDB.ts
@@ -14,8 +14,23 @@ export default class PaginationDB implements PaginationRepository {
       const query =
         "SELECT * FROM docs WHERE ?? LIKE ? AND status = 'available' LIMIT 5 OFFSET ?";
       const offset = page * 5;
-      const output = await this.db.query(query, [type, `%${value}%`, offset]);
-      return output[0];
+      const [output] = await this.db.query(query, [type, `%${value}%`, offset]);
+      return output;
+    } catch (error) {
+      if (error instanceof Error) throw new Error(error.message);
+      throw new Error("Unexpected error DB");
+    } finally {
+      await this.db.close();
+    }
+  }
+  async filterDocumentById(id: string): Promise<any> {
+    try {
+      await this.db.connect();
+      const [output] = await this.db.query(
+        "SELECT * FROM docs WHERE id = ? AND status = 'available'",
+        [id]
+      );
+      return output;
     } catch (error) {
       if (error instanceof Error) throw new Error(error.message);
       throw new Error("Unexpected error DB");
