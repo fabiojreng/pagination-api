97fb985ee0e8d696f15c52b2bd7379f3a0793e8e
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-01-24 16:28:01 -0300

    feat: change parameter of callback

--- src/infra/http/AdapterExpress.ts
+++ src/infra/http/AdapterExpress.ts
@@ -12,7 +12,7 @@ export default class AdapterExpress implements HttpServer {
   register(method: string, url: string, callback: Function): void {
     return this.app[method](url, async function (req: Request, res: Response) {
       try {
-        const { statusCode, body } = await callback(req.query);
+        const { statusCode, body } = await callback(req);
         res.status(statusCode).json(body);
       } catch (error) {
         if (error instanceof Error) res.status(400).json(error.message);
