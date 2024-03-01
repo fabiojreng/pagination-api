694e169a8553d547ffe0655787126a898b8c0198
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-03-01 19:24:47 -0300

    refactor

--- src/infra/http/AdapterExpress.ts
+++ src/infra/http/AdapterExpress.ts
@@ -1,25 +1,27 @@
 import express, { Request, Response } from "express";
 import HttpServer from "./HttpServer";
+import HttpResponse from "../../domain/entities/Protocols/HttpResponse";
+//import cors from "cors";
 
 export default class AdapterExpress implements HttpServer {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   app: any;
   constructor() {
     this.app = express();
     this.app.use(express.json());
+    //this.app.use(cors());
   }
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  register(method: string, url: string, callback: Function): void {
+  register(method: string, url: string, callback: Function): any {
     return this.app[method](url, async function (req: Request, res: Response) {
       try {
-        const { statusCode, body } = await callback(req);
-        res.status(statusCode).json(body);
+        const output: HttpResponse = await callback(req);
+        res.status(output.statusCode).json(output.body);
       } catch (error) {
         if (error instanceof Error) res.status(400).json(error.message);
         res.status(400).json("Unexpected error");
       }
     });
   }
+
   listen(port: number): void {
     this.app.listen(port, () => {
       console.log(`SERVER RUNNING IN PORT ${port}`);
