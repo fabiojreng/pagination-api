0f172616a7f081bd3116a2be802fde0f5c7a9ed2
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-03-01 19:26:18 -0300

    feat: create document count route

---
+++ src/application/useCases/CountDocumentsUseCase.1.ts
@@ -0,0 +1,30 @@
+import MissingParamError from "../../domain/entities/Errors/MissingParamError";
+import NotFoundError from "../../domain/entities/Errors/NotFoundError";
+import {
+  notFound,
+  serverError,
+  success,
+} from "../../domain/entities/Helpers/HttpHelper";
+import HttpResponse from "../../domain/entities/Protocols/HttpResponse";
+import PaginationRepository from "../repository/PaginationRepository";
+import UseCase from "./UseCase";
+
+export default class CountDocumentsUseCase implements UseCase {
+  constructor(private repository: PaginationRepository) {}
+  async execute(params: Input): Promise<HttpResponse> {
+    try {
+      const permittedTypes = ["type_document", "area_CNPQ"];
+      if (!permittedTypes.includes(params.type))
+        return notFound(new MissingParamError("type_document or area_CNPQ"));
+      const data = await this.repository.countDocuments(params.type);
+      if (!data) return notFound(new NotFoundError());
+      return success({ message: "", data: data });
+    } catch (error) {
+      if (error instanceof Error) return serverError(error);
+      return serverError(new Error("Unexpected Error"));
+    }
+  }
+}
+type Input = {
+  type: string;
+};
