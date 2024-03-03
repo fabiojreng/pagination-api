a8503ddb65c9d8a217ef9b39c35d04747a8c9615
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-03-02 22:03:24 -0300

    refactor

--- src/infra/controller/MainController.ts
+++ src/infra/controller/MainController.ts
@@ -13,15 +13,15 @@ export default class MainController {
       const output = await this.filterDocuments.execute(req.query);
       return output;
     });
-    this.server?.register("get", "/getById/:id", async (req: any) => {
+    this.server?.register("get", "/get-by-Id/:id", async (req: any) => {
       const output = await this.getById.execute(req.params.id);
       return output;
     });
-    this.server?.register("get", "/countDocuments", async (req: any) => {
+    this.server?.register("get", "/count-documents", async (req: any) => {
       const output = await this.countDocuments.execute(req.query);
       return output;
     });
-    this.server?.register("get", "/latestPosts", async () => {
+    this.server?.register("get", "/latest-posts", async () => {
       const output = await this.latestPosts.execute({});
       return output;
     });
--- src/infra/repository/PaginationDB.ts
+++ src/infra/repository/PaginationDB.ts
@@ -10,10 +10,16 @@ export default class PaginationDB implements PaginationRepository {
     page: number
   ): Promise<any> {
     await this.db.connect();
+    // const [qtdDocuments] = await this.db.query(
+    //   "SELECT COUNT(*) AS quantidade FROM documents WHERE status = 'available'",
+    //   []
+    // );
+    // console.log(qtdDocuments);
     const query =
       "SELECT d.*, f.file, f.file_name, f.file_size FROM documents d LEFT JOIN files f ON d.id = f.id_document WHERE ?? LIKE ? AND status = 'available' LIMIT 5 OFFSET ?;";
     const offset = page * 5;
     const [output] = await this.db.query(query, [type, `%${value}%`, offset]);
+    await this.db.close();
     return output;
   }
 
@@ -23,6 +29,7 @@ export default class PaginationDB implements PaginationRepository {
       "SELECT * FROM documents WHERE id = ? AND status = 'available'",
       [id]
     );
+    await this.db.close();
     return output;
   }
 
@@ -30,6 +37,7 @@ export default class PaginationDB implements PaginationRepository {
     await this.db.connect();
     const query = `SELECT ${type}, COUNT(*) AS quantidade FROM documents WHERE status = 'available' GROUP BY ${type}`;
     const [output] = await this.db.query(query, []);
+    await this.db.close();
     return output;
   }
 
@@ -38,6 +46,7 @@ export default class PaginationDB implements PaginationRepository {
     const query =
       "SELECT * FROM documents WHERE status = 'available' ORDER BY updated_at DESC LIMIT 5";
     const [output] = await this.db.query(query, []);
+    await this.db.close();
     return output;
   }
 }
